032f9f7eab6d45ce367a6b58e952a212d1b9a081
cs02433 <cs02433@gmail.com>
2020-03-09 16:09:36 +0530

    First draft check ins

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SprintCapacityPlanner from './sprint-capacity-planner/SprintCapacityPlanner';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const members = [];
+const fixedTasks = [];
+
+ReactDOM.render(<SprintCapacityPlanner members={members} addMember={addMember}/>, document.getElementById('root'));
+
+function addFixedTask(task) {
+    fixedTasks.push(task);
+    render();
+}
+
+function addMember(member) {
+    members.push(member);
+    render();
+}
+
+function render() {
+    ReactDOM.render(<SprintCapacityPlanner getTeamData={getTeamData} addFixedTask={addFixedTask} fixedTasks={fixedTasks}
+                                           members={members}
+                                           addMember={addMember}/>, document.getElementById('root'));
+}
+
+function getTeamData() {
+    let fixedTask = fixedTasks.map(t => t.estimates).reduce((a, b) => a + b, 0);
+    return {
+        members: members,
+        publicHolidays: 0,
+        fixedTasks: fixedTask
+    };
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
